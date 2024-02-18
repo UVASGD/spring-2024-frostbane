5dae091bf24f6fe430599b22efb6b643b007a7c0
Phong Le <le.phonghoa105@gmail.com>
2024-02-18 15:21:07 -0500

    file path

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -7,7 +7,7 @@ func _ready():
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://3dLevel.tscn")
+	get_tree().change_scene_to_file("res://Scenes/3dLevel.tscn")
 
 
 func _on_quit_button_pressed():
