442fb7446af60c81b5618681ba490ec7e47b1ff0
VincentChan22 <VincentHChan22@gmail.com>
2024-04-02 17:34:43 -0400

    Fog excluder and gamemanager work
    Fog is removed in the direction the player is looking. The fog gets worse as time goes on.

--- Scenes/GameManager.gd
+++ Scenes/GameManager.gd
@@ -1,8 +1,12 @@
 extends Node
 
 static var gameManager
-@export var timeBetween :float
-@export var maxEmission :float
+@export var totalTime:int
+@export var minFog :float
+@export var maxFog :float
+@onready var timer = $Timer 
+@export var wE :WorldEnvironment
+var curTime:int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if(gameManager == null):
@@ -10,8 +14,20 @@ func _ready():
 	else:
 		printerr("2 Gamemanager objects detected")
 		queue_free()
+		
+	wE.environment.volumetric_fog_density = minFog
+	timer.start();
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 	
+
+
+func _on_timer_timeout():
+	if(curTime < totalTime):
+		wE.environment.volumetric_fog_density = float(curTime)/totalTime * (maxFog - minFog)
+		print("TickTock")
+		curTime+=1
+
